6133ff30a53682602cfac49db8ab1ea082fcb2b5
Almarales24 <almarales24@example.com>
2026-05-22 22:43:20 -0500

    feat: Crear componente base CalculadoraPrestamos
    - Componente TypeScript con selector app-calculadora-prestamos
    - Template y estilos separados
    - Listo para agregar lógica

---
+++ FinanCalc/src/app/components/calculadora-prestamos/calculadora-prestamos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora-prestamos',
+  imports: [],
+  templateUrl: './calculadora-prestamos.html',
+  styleUrl: './calculadora-prestamos.css',
+})
+export class CalculadoraPrestamos {}
